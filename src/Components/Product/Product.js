6f5f8bf0b771685aac6ae2688735c37b8af8e60e
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-25 09:15:58 +0600

    made some styling

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const{name,category,seller,img,price,rating}=props.product
+    const{name,category,seller,img,price,ratings}=props.product
     return (
         <div className='product'>
             <img className='img' src={img} alt="" />
-            
+            <div className="details">
+                <h4>{name}</h4>
+                <h5>Price : {price}</h5>
+                <p><small>Category : {category}</small></p>
+                <p><small>Rating: {ratings}</small></p>
+            </div>
+            <button className='cart-btn'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
